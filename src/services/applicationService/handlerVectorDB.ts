6ed5b7f1a034efb5ccd7d9c567dd30a87585c420
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-11 20:13:17 -0300

    feat: implements handlerVectorDB.ts

---
+++ src/services/applicationService/handlerVectorDB.ts
@@ -0,0 +1,77 @@
+/* eslint-disable prettier/prettier */
+import prisma from "../../lib/prisma";
+import { PrismaVectorStore } from "langchain/vectorstores/prisma";
+import { OllamaEmbeddings } from "langchain/embeddings/ollama";
+import { Prisma, Document as PrismaDocument } from "@prisma/client";
+import { Document } from "langchain/dist/document";
+
+export const save = async (embedDocs: Document<Record<string, any>>[]) => {
+  try {
+    const vectorStore = PrismaVectorStore.withModel<PrismaDocument>(
+      prisma,
+    ).create(new OllamaEmbeddings({ model: "nomic-embed-text:latest" }), {
+      prisma: Prisma,
+      tableName: "Document",
+      vectorColumnName: "vector",
+      columns: {
+        id: PrismaVectorStore.IdColumn,
+        content: PrismaVectorStore.ContentColumn,
+      },
+    });
+
+    await vectorStore.addModels(
+      await prisma.$transaction(
+        embedDocs.map((content) =>
+          prisma.document.create({
+            data: { content: content.pageContent },
+          }),
+        ),
+      ),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const search = async (question: string) => {
+  try {
+    const vectorStore = new PrismaVectorStore(
+      new OllamaEmbeddings({ model: "nomic-embed-text:latest" }),
+      {
+        db: prisma,
+        prisma: Prisma,
+        tableName: "Document",
+        vectorColumnName: "vector",
+        columns: {
+          id: PrismaVectorStore.IdColumn,
+          content: PrismaVectorStore.ContentColumn,
+        },
+      },
+    );
+
+    const documents = await vectorStore.similaritySearch(question, 10);
+
+    const context = documents.filter((document) => {
+      if (document.metadata?._distance) {
+        return (
+          Number(
+            document.metadata._distance
+              .toString()
+              .match(/^-?\d+(\.\d{1,2})?/)?.[0] || 0,
+          ) <= 0.24
+        );
+      }
+    });
+
+    return context;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const pdfService = {
+  save,
+  search,
+};
+
+export default pdfService;
